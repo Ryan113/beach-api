99107d0e853ddcb392cd3112515ba4fab1687e17
Ryan <>
2018-10-03 16:49:39 -0700

    added appropriate routes for individual houses

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const ical = require('ical');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const url = 'http://admin.vrbo.com/icalendar/ae077e5319bf4ce68271d4476a562cba.ics';
-
 var getDaysArray = function(start, end) {
     for(var arr=[],dt=start; dt<end; dt.setDate(dt.getDate()+1)){
         arr.push(new Date(dt).toISOString().substring(0,10));
@@ -13,8 +11,55 @@ var getDaysArray = function(start, end) {
     return arr;
 };
 
-app.get('/', (req, res) => {
+app.get('/beach-house', (req, res) => {
 
+  const url = 'http://admin.vrbo.com/icalendar/ae077e5319bf4ce68271d4476a562cba.ics';
+  const dates = [];
+
+  ical.fromURL(url, {}, function(err, data) {
+    for (var k in data) {
+      //console.log(data[k].start);
+      const start = data[k].start.toISOString().substring(0,10);
+      const end = data[k].end.toISOString().substring(0,10);
+      const bookedDates = getDaysArray(new Date(start),new Date(end));
+
+      dates.push({
+          title: 'reserved',
+          start: start,
+          end: end,
+          bookedDates: bookedDates
+      });
+    };
+    res.send(dates);
+  });
+});
+
+app.get('/sea-grass', (req, res) => {
+
+  const url = 'http://admin.vrbo.com/icalendar/200274e35ffe48008c094213e27567a1.ics';
+  const dates = [];
+
+  ical.fromURL(url, {}, function(err, data) {
+    for (var k in data) {
+      //console.log(data[k].start);
+      const start = data[k].start.toISOString().substring(0,10);
+      const end = data[k].end.toISOString().substring(0,10);
+      const bookedDates = getDaysArray(new Date(start),new Date(end));
+
+      dates.push({
+          title: 'reserved',
+          start: start,
+          end: end,
+          bookedDates: bookedDates
+      });
+    };
+    res.send(dates);
+  });
+});
+
+app.get('/ocean-trail', (req, res) => {
+
+  const url = 'https://www.airbnb.com/calendar/ical/12713758.ics?s=717c33ba6048f7ce586aaa66a9795911';
   const dates = [];
 
   ical.fromURL(url, {}, function(err, data) {
