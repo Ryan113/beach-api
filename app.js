4a7ff5e29424e67ca5c6579611e0932c3ee18a82
Ryan <>
2018-10-03 19:36:38 -0700

    update to port

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var getDaysArray = function(start, end) {
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', port);
+    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:8000');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
